00261787339a1cd80ff8088155d952aa998a276e
Shahab Dogar <shahab96@gmail.com>
2022-08-06 15:55:14 -0700

    fix: Give lambda permission to write to cloudwatch

--- rules/infrastructure/terraform/role.tf
+++ rules/infrastructure/terraform/role.tf
@@ -14,6 +14,11 @@ resource "aws_iam_role" "this" {
   assume_role_policy = data.aws_iam_policy_document.assume.json
 }
 
+resource "aws_iam_role_policy_attachment" "this" {
+  role       = aws_iam_role.this.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 data "aws_iam_policy_document" "permissions" {
   statement {
     effect = "Allow"
