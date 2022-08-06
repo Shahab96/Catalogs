20054ff046df5bfbeb23b7f857417f47de7bb77d
Shahab Dogar <shahab96@gmail.com>
2022-08-06 16:02:58 -0700

    fix: Switch to proxy api

--- rules/infrastructure/terraform/api.tf
+++ rules/infrastructure/terraform/api.tf
@@ -1,14 +1,23 @@
-data "template_file" "this" {
-  template = file("api.yaml")
-  vars = {
-    lambda_arn = "${aws_lambda_function.this.arn}"
-  }
-}
-
 resource "aws_apigatewayv2_api" "this" {
   name          = local.project_prefix
   protocol_type = "HTTP"
   body          = data.template_file.this.rendered
+}
+
+resource "aws_apigatewayv2_integration" "this" {
+  api_id                    = aws_apigatewayv2_api.this.id
+  integration_type          = "AWS"
+  connection_type           = "INTERNET"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  integration_method        = "POST"
+  integration_uri           = aws_lambda_function.this.invoke_arn
+  passthrough_behavior      = "WHEN_NO_MATCH"
+}
+
+resource "aws_apigatewayv2_route" "this" {
+  api_id    = aws_apigatewayv2_api.this.id
+  route_key = "ANY /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
 }
 
 resource "aws_apigatewayv2_stage" "this" {
