4d3bbe8dcf675d6bdd0936158558ceeca4a30180
Shahab Dogar <shahab@dogar.dev>
2022-09-03 17:49:31 -0700

    feat/UserService (#10)
    * feat: Create user service with /register endpoint
    
    * chore: Add @Shahab96 as owner for user service
    
    * fix: Update build workflow to use directory of updated service
    
    * Revert "fix: Update build workflow to use directory of updated service"
    
    This reverts commit 54d38486708c692130853edf74348cbb576693bc.
    
    * fix: Made build executable
    
    * fix: Add better error handling to register endpoint for when user already exists
    
    * feat: Add login endpoint, mint jwt on successful login.
    
    * fix: Conflict in terraform.lock
    
    Co-authored-by: Shahab Dogar <shahab96@gmail.com>

--- user/src/model/user.rs
+++ user/src/model/user.rs
@@ -1,10 +1,11 @@
-use aws_sdk_dynamodb::model::AttributeValue;
-use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::model::{AttributeValue, ReturnValue};
+use aws_sdk_dynamodb::output::{PutItemOutput, UpdateItemOutput};
 use rocket::State;
 use serde::Serialize;
 use uuid::Uuid;
 
 use crate::utils::utils::hash_password;
+use crate::model::state;
 
 #[derive(Debug, Serialize)]
 pub struct User {
@@ -23,7 +24,7 @@ impl User {
         let pk = format!("USER#{}", email);
         let gsi_uuid = format!("UUID#{}", uuid);
         let email = String::from(email);
-        let hashed_password = hash_password(password);
+        let hashed_password = hash_password(password).to_string();
         let mut tenant_list = Vec::new();
         tenant_list.push(Uuid::new_v4().to_string());
         let active_tenant = String::from("");
@@ -41,12 +42,11 @@ impl User {
 
     pub async fn create(
         user: &Self,
-        client: &State<Client>,
-        table_name: &str,
-    ) -> Result<(), String> {
-        let result = client
+        state: &State<state::State>,
+    ) -> Result<PutItemOutput, aws_sdk_dynamodb::Error> {
+        let result = state.dynamo
             .put_item()
-            .table_name(table_name)
+            .table_name(&state.table_name)
             .item("pk", AttributeValue::S(user.pk.clone()))
             .item("gsi_uuid", AttributeValue::S(user.gsi_uuid.clone()))
             .item("uuid", AttributeValue::S(user.uuid.to_string()))
@@ -56,15 +56,47 @@ impl User {
                 AttributeValue::S(user.hashed_password.clone()),
             )
             .item("tenant_list", AttributeValue::Ss(user.tenant_list.clone()))
-            .item("active_tenant", AttributeValue::S(user.active_tenant.clone()))
+            .item(
+                "active_tenant",
+                AttributeValue::S(user.active_tenant.clone()),
+            )
+            .condition_expression("attribute_not_exists(email)")
             .send()
-            .await;
+            .await?;
 
-        match result {
-            Ok(_) => Ok(()),
-            Err(error) => match error {
-                _ => Err(error.to_string()),
-            },
-        }
+        Ok(result)
+    }
+
+    pub async fn login(
+        email: &str,
+        state: &State<state::State>,
+    ) -> Result<UpdateItemOutput, aws_sdk_dynamodb::Error> {
+        let user_pk = format!("USER#{}", email);
+
+        let tenant = state.dynamo
+            .query()
+            .table_name(&state.table_name)
+            .index_name("email-index")
+            .key_condition_expression("gsi_email = :email")
+            .expression_attribute_values(":email", AttributeValue::S(format!("EMAIL#{}", email)))
+            .send()
+            .await?;
+
+        let tenant_id = match tenant.items() {
+            Some(t) => t[0].get("uuid").unwrap().as_s().unwrap().to_string(),
+            None => String::from(""),
+        };
+
+        let response = state.dynamo
+            .update_item()
+            .table_name(&state.table_name)
+            .key("pk", AttributeValue::S(user_pk))
+            .update_expression("SET active_tenant = :tenant_id")
+            .expression_attribute_values(":tenant_id", AttributeValue::S(tenant_id))
+            .return_values(ReturnValue::AllNew)
+            .send()
+            .await?;
+
+        Ok(response)
     }
 }
--- user/src/routes/user.rs
+++ user/src/routes/user.rs
@@ -1,29 +1,67 @@
-use aws_sdk_dynamodb::Client;
+use aws_sdk_dynamodb::Error::ConditionalCheckFailedException;
 use rocket::http::Status;
-use rocket::response::status::Created;
+use rocket::response::status::{Accepted, Created, Custom};
 use rocket::serde::json::Json;
 use rocket::{post, State};
-use serde::{Serialize, Deserialize};
+use serde::Deserialize;
 
 use crate::model::user::User;
+use crate::model::tenant::Tenant;
+use crate::model::state;
+use crate::utils::utils::{verify_password, mint_token};
 
-#[derive(Serialize, Deserialize)]
-pub struct RegisterRequest<'a> {
+#[derive(Deserialize)]
+pub struct ClientRequest<'a> {
     email: &'a str,
     password: &'a str,
 }
 
 #[post("/register", data = "<data>", format = "json")]
 pub async fn register(
-    client: &State<Client>,
-    table_name: &State<String>,
-    data: Json<RegisterRequest<'_>>,
-) -> Result<Created<()>, Status> {
-    let tenant = User::new(data.email, data.password);
-    let result = User::create(&tenant, client, &table_name).await;
+    state: &State<state::State>,
+    data: Json<ClientRequest<'_>>,
+) -> Result<Created<()>, Custom<&'static str>> {
+    let user = User::new(data.email, data.password);
+    let result = User::create(&user, state).await;
 
     match result {
-        Ok(_) => Ok(Created::new("/register")),
-        Err(_) => Err(Status::InternalServerError),
+        Ok(_) => {
+            let tenant = Tenant::new(&user.email, &user.tenant_list.first().unwrap());
+            match Tenant::create(&tenant, state).await {
+                Ok(_) => Ok(Created::new("/")),
+                Err(_) => Err(Custom(Status::InternalServerError, "Error creating your account.")),
+            }
+        },
+        Err(err) => match err {
+            ConditionalCheckFailedException(_) => {
+                Err(Custom(Status::Conflict, "User already exists."))
+            }
+            _ => Err(Custom(Status::InternalServerError, "Internal Server Error")),
+        },
+    }
+}
+
+#[post("/login", data = "<data>", format = "json")]
+pub async fn login<'a> (
+    state: &State<state::State>,
+    data: Json<ClientRequest<'_>>,
+) -> Result<Accepted<String>, Custom<&'a str>> {
+    let result = User::login(data.email, state).await;
+
+    match result {
+        Ok(user) => {
+            if verify_password(data.password) {
+                let tenant_id = user.attributes().unwrap().get("active_tenant").unwrap().as_s().unwrap();
+                let jwt = mint_token(&state.access_token, &data.email, &tenant_id).unwrap();
+                
+                Ok(Accepted(Some(String::from(jwt.as_str()))))
+            } else {
+                Err(Custom(Status::Unauthorized, "Incorrect email or password."))
+            }
+        },
+        Err(err) => match err {
+            ConditionalCheckFailedException(_) => Err(Custom(Status::NotFound, "Incorrect email or password.")),
+            _ => Err(Custom(Status::InternalServerError, "Internal Server Error")),
+        },
     }
 }
